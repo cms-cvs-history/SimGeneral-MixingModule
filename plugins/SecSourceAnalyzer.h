2f6bcd0bcfe81fdcfee209ff00da72ebba054e57
Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch>
2011-07-11 19:46:02 +0000

    Update to new VectorInputSource with memory improvements.

--- plugins/SecSourceAnalyzer.h
+++ plugins/SecSourceAnalyzer.h
@@ -8,7 +8,7 @@
 //
 // Original Author:  Emilia Lubenova Becheva
 //         Created:  Wed Apr 22 16:54:31 CEST 2009
-// $Id: SecSourceAnalyzer.h,v 1.1 2009/04/24 13:35:34 ebecheva Exp $
+// $Id: SecSourceAnalyzer.h,v 1.2 2010/04/30 12:48:18 ebecheva Exp $
 //
 //
 
@@ -35,21 +35,17 @@
 
 
 //
-// class decleration
+// class declaration
 //
 namespace edm
 {
 class SecSourceAnalyzer : public edm::EDAnalyzer {
    public:
    
-      typedef PileUp::EventPrincipalVector EventPrincipalVector;
-      
       explicit SecSourceAnalyzer(const edm::ParameterSet&);
       ~SecSourceAnalyzer();
 
-      virtual void Loop(const EventPrincipalVector& vec);
-
-      virtual void getBranches(EventPrincipal *ep);
+      virtual void getBranches(EventPrincipal const& ep);
 
    private:
       virtual void beginJob() ;
@@ -57,18 +53,13 @@ class SecSourceAnalyzer : public edm::EDAnalyzer {
       virtual void endJob() ;
 
       // ----------member data ---------------------------
-      int minBunch_;
-      int maxBunch_;
-      
       bool dataStep2_;
       edm::InputTag label_;
       
-      std::vector<std::vector<edm::EventID> > vectorEventIDs_;
+      std::vector<edm::EventID> vectorEventIDs_;
 
       boost::shared_ptr<PileUp> input_;
 
-      std::vector<EventPrincipalVector> pileup_[5];
-       
       InputTag tag_;
  
 };
