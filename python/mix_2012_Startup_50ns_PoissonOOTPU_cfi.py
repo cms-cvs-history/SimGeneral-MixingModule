0027a38fceabd3965ca6602b717c2fa1652f9e08
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-04-25 15:44:33 +0000

    Initial changes for pileup memory reduction

--- python/mix_2012_Startup_50ns_PoissonOOTPU_cfi.py
+++ python/mix_2012_Startup_50ns_PoissonOOTPU_cfi.py
@@ -3,8 +3,21 @@ import FWCore.ParameterSet.Config as cms
 # configuration to model pileup for initial physics phase
 from SimGeneral.MixingModule.mixObjects_cfi import * 
 from SimGeneral.MixingModule.mixPoolSource_cfi import * 
+from SimGeneral.MixingModule.pixelDigitizer_cfi import *
+from SimGeneral.MixingModule.stripDigitizer_cfi import *
+from SimGeneral.MixingModule.ecalDigitizer_cfi import *
+from SimGeneral.MixingModule.hcalDigitizer_cfi import *
+from SimGeneral.MixingModule.castorDigitizer_cfi import *
+
 
 mix = cms.EDProducer("MixingModule",
+    digitizers = cms.VPSet(
+      pixelDigitizer,
+      stripDigitizer,
+      ecalDigitizer,
+      hcalDigitizer,
+      castorDigitizer
+    ),
     LabelPlayback = cms.string(''),
     maxBunch = cms.int32(3),
     minBunch = cms.int32(-2), ## in terms of 25 nsec
