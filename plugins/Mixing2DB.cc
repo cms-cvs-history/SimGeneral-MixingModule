76a4a782db1c012c6559cc729604b1a2a60199b4
cmsbuild <>
2013-04-04 15:12:59 +0000

    This commit was manufactured by cvs2git to create tag 'SimGeneral- MixingModule-V06-02-01-06'.
    Sprout from SimGeneral-MixingModule-V06-02-01_newPileup 2013-01-30 17:23:07 UTC Mike Hildreth <sha1-869212f33f5a723e6a1701e0899b24f90335ecd8@cern.ch> 'add 2012 25ns pileup scenarios'
    Cherrypick from SimGeneral-MixingModule-gf_53xBranch 2013-04-04 15:12:58 UTC Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch> 'update to ease IOV's for run dependent MC':
        plugins/Mixing2DB.cc

--- plugins/Mixing2DB.cc
+++ plugins/Mixing2DB.cc
@@ -104,8 +104,7 @@ Mixing2DB::endJob()
   edm::Service<cond::service::PoolDBOutputService> poolDbService;
   MixingModuleConfig * config = new MixingModuleConfig();
   config->read(cfi_);
-  poolDbService->createNewIOV<MixingModuleConfig>(config,
-						  poolDbService->beginOfTime(),poolDbService->endOfTime(),
-						  "MixingRcd");
-
+  poolDbService->writeOne<MixingModuleConfig>(config,
+					      poolDbService->currentTime(),
+					      "MixingRcd");
 }
